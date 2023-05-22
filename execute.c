d2e310a3ffe9c180b8d6e5b4b66705962a865cef
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-22 14:13:00 +0300

    insecption of 0x19

---
+++ execute.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * execute - find the desired opcode and call the related funciton
+ * @content: a single line in the monty file
+ * @stack: pointer to
+ * @counter:
+ * @fp:
+ *
+ */
+void execute(char *content, stack_t *stack, unsigned int counter, FILE fp)
+{
+
+}
---
+++ main.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+info = {NULL, NULL, NULL, 0};
+/**
+ * main - entry point of the program
+ * @arc: count of arguments provided by user
+ * @argv: array of argumbers provide by user
+ * Return: 0 on success, 1 of faliur
+ */
+int main (int argc, char *argv[])
+{
+FILE *file;
+char *content;
+size_t len;
+ssize_t read_line;
+
+content = NULL;
+read_line = 0;
+if (argc != 2)
+{
+    fprintf(stderr, "Usage: executable montyfilename");
+    exit(EXIT_FAILURE);
+}
+file = fopen(argv[1], "r");
+info.file = file;
+if (file == NULL)
+{
+    fprintf(stderr, "File can not be opend");
+    exit(EXIT_FAILURE);
+}
+/* start reading lines from monty file*/
+read_line = getline(&content, &len, file);
+printf("size of \tlen = %lu\tread: %ld", len, read_line);
+return (0);
+}
