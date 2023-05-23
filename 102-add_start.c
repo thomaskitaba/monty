551d2a4008941dc07ce0e9b491eede8fd7dc9a1c
thomaskitaba <thomaskitaba@gmail.com>
2023-05-23 23:12:24 +0300

    confilic resolved

---
+++ 102-add_start.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+* add_end - add node at the end of d_list
+* @head: head of the doublec
+* @n: number to be inserted
+* Return: inserted node address, or NUll
+*/
+stack_t *add_start(stack_t **head, int n)
+{
+
+	return (*head);
+}
---
+++ 102-pop_end.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+* pop_end - delete last node at the end of d_list
+* @head: head of the doublec
+* @n: number to be inserted
+* Return: inserted node address, or NUll
+*/
+stack_t *pop_end(stack_t **head, unsigned int n)
+{
+
+    return (*head);
+}
