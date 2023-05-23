80c4ee46e8437671414eae93a7b7aeff8585017e
thomaskitaba <thomaskitaba@gmail.com>
2023-05-23 12:41:45 +0300

    removed segfault inside execute.c

--- main.c
+++ main.c
@@ -35,7 +35,6 @@ if (file == NULL)
 /* start reading lines from monty file*/
 while (read_line != -1)
 {
-    content = NULL;
     read_line = getline(&content, &len, file);
     if (ferror(file))
     {
@@ -47,9 +46,9 @@ while (read_line != -1)
     if (read_line > 0)
         execute(content, &stack, l_count, file);
     /*printf("size of \tlen = %lu\tread: %ld\n", len, read_line);*/
-    free(content);
 }
-
+free(content);
+free(stack);
 fclose(file);
 return (0);
 }
