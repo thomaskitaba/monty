dd913a351cb910c442fbc71307bae4f9e5c22faf
root <root@ThomasKitaba.localdomain>
2023-05-24 20:35:48 +0300

    update implicit declereation

--- 0-sq_push.c
+++ 0-sq_push.c
@@ -7,7 +7,34 @@
 */
 void sq_push(stack_t **head, unsigned int counter)
 {
-    size_t i;
+    int n, j = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		add_start(head, n)
+	else
+		add_start(head, n);
+   /* size_t i;
 
     i = 0;
     if (head == NULL)
@@ -37,5 +64,5 @@ void sq_push(stack_t **head, unsigned int counter)
     if (info.ord_type == 0)
         add_start(head, atoi(info.arg));
     else
-        add_start(head, atoi(info.arg));
+        add_start(head, atoi(info.arg));*/
 }
