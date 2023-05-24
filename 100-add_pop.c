49c1c0c7c4b37bbd49e894e3383b4cf4f199b93d
thomaskitaba <thomaskitaba@gmail.com>
2023-05-24 11:11:42 +0300

    stage 2: created 100-add_pop.c

---
+++ 100-add_pop.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+/**
+* add_end - add node at the end of d_list
+* @head: head of the doublec
+* @n: number to be inserted
+* Return: inserted node address, or NUll
+*/
+stack_t *add_start(stack_t **head, int n)
+{
+
+	return (*head);
+}
+
+/**
+* add_end - add node at the end of d_list
+* @head: head of the doublec
+* @n: number to be inserted
+* Return: inserted node address, or NUll
+*/
+stack_t *add_start(stack_t **head, int n)
+{
+
+	return (*head);
+}
+/**
+* pop_start - delete the top node at the begning
+* @head: head of the double linked list
+* @n: number to be inserted to the list
+* Return: head of the node, or Null
+*/
+stack_t *pop_start(stack_t **head, unsigned int n)
+{
+
+	return (*head);
+}
+/**
+* pop_end - delete last node at the end of d_list
+* @head: head of the doublec
+* @n: number to be inserted
+* Return: inserted node address, or NUll
+*/
+stack_t *pop_end(stack_t **head, unsigned int n)
+{
+
+    return (*head);
+}
