ca3fd465d7c7b4d76df57697ddeb736233f0b017
root <root@ThomasKitaba.localdomain>
2023-05-24 16:55:51 +0300

    update

--- 4-sq_swap.c
+++ 4-sq_swap.c
@@ -5,7 +5,7 @@
 * @counter: line number of the code
 * Return: nothing
 */
-void sq_swap(stack_t **stack, unsigned int counter)
+void sq_swap(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int counter)
 {
     printf("sq_swap funciton goes here\n");
 }
