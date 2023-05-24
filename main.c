8c935f9ab0ce460b7c2b22cd812e6242230267cd
root <root@ThomasKitaba.localdomain>
2023-05-24 20:27:18 +0300

    update getline implicit decleration

--- main.c
+++ main.c
@@ -10,47 +10,48 @@ stack_t *stack = NULL;
  */
 int main (int argc, char *argv[])
 {
-FILE *file;
-char *content;
-size_t len;
-int read_line;
-unsigned int l_count;
-content = NULL;
-read_line = 0;
-l_count = 0;
-if (argc != 2)
-{
-    fprintf(stderr, "USAGE: monty file\n");
-    exit(EXIT_FAILURE);
-}
-file = fopen(argv[1], "r");
-info.file = file;
-if (file == NULL)
-{
-    fprintf(stderr, "Error: Can't open file <%s>\n", argv[1]);
-    exit(EXIT_FAILURE);
-}
-/* start reading lines from monty file*/
-while (read_line != -1)
-{
+    FILE *file;
+    char *content;
+    size_t len;
+    int read_line;
+    unsigned int l_count;
     content = NULL;
-    read_line = (int)getline(&content, &len, file);
-    if (ferror(file))
+    read_line = 0;
+    l_count = 0;
+    if (argc != 2)
     {
-        fprintf(stderr, "Error: error while reading file\n");
-        free(content);
-        fclose(file);
-        free_stack(stack);
+        fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
-    l_count++;
-    info.content = content;
-    if (read_line > 0)
-        execute(content, &stack, l_count, file);
-    /*printf("size of \tlen = %lu\tread: %ld\n", len, read_line);*/
-    free(content);
-}
-free_stack(stack);
-fclose(file);
-return (0);
+    file = fopen(argv[1], "r");
+    info.file = file;
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file <%s>\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    /* start reading lines from monty file*/
+    while (read_line > 0)
+    {
+        content = NULL;
+        read_line = (int)getline(&content, &len, file);
+        if (ferror(file))
+        {
+            fprintf(stderr, "Error: error while reading file\n");
+            free(content);
+            fclose(file);
+            free_stack(stack);
+            exit(EXIT_FAILURE);
+        }
+        l_count++;
+        info.content = content;
+        if (read_line > 0)
+        {
+            execute(content, &stack, l_count, file);
+        }
+        free(content);
+    }
+    free_stack(stack);
+    fclose(file);
+    return (0);
 }
