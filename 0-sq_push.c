7b90c162ee10fa4ceb40aa2d82cd549914b9c60d
thomaskitaba <thomaskitaba@gmail.com>
2023-05-24 13:45:20 +0300

    stage 2: added extern stack_t stack

--- 0-sq_push.c
+++ 0-sq_push.c
@@ -5,21 +5,21 @@
 * @counter: line number of the code
 * Return: nothing
 */
-void sq_push(stack_t **stack, unsigned int counter)
+void sq_push(stack_t **head, unsigned int counter)
 {
     stack_t *current;
-    current = *stack;
+    current = *head;
     printf("sq_push funciton goes here\n");
     if (stack == NULL)
     {
         fprintf(stderr, "Invaled argument passed");
         fclose(info.file);
         free(info.content);
-        free_stack(*stack);
+        free_stack(*head);
         exit(EXIT_FAILURE);
     }
     if (info.ord_type == 0)
-        add_start(stack, atoi(info.arg));
+        add_start(head, atoi(info.arg));
     else
-        add_start(stack, atoi(info.arg));
+        add_start(head, atoi(info.arg));
 }
